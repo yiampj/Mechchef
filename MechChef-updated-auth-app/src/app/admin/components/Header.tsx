23436c7cb7cbba3906be7bd5bad9dc1b6ac7d5f8
yiampj <iamjayprajapati@gmail.com>
2026-05-18 00:51:08 +0100

    Create Header.tsx

---
+++ MechChef-updated-auth-app/src/app/admin/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header style={{background: '#1a1a18', padding: '1rem'}}>
+      <h1 style={{color: '#D4A843'}}>MechChef</h1>
+    </header>
+  )
+}
